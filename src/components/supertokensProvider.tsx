20ae3931b36f4e198ca9b1e7a3e876d4bc60df8b
gernesto14 <gernesto14@yahoo.com>
2025-03-09 00:04:43 -0500

    Test

--- src/components/supertokensProvider.tsx
+++ src/components/supertokensProvider.tsx
@@ -1,15 +1,11 @@
-// disable lint
 /* eslint-disable */
-
 "use client";
+
 import React from "react";
 import { SuperTokensWrapper } from "supertokens-auth-react";
 import SuperTokensReact from "supertokens-auth-react";
 import { frontendConfig, setRouter } from "../config/frontend";
 import { usePathname, useRouter } from "next/navigation";
-import { EmailVerificationPreBuiltUI } from "supertokens-auth-react/recipe/emailverification/prebuiltui";
-import { canHandleRoute, getRoutingComponent } from "supertokens-auth-react/ui";
-import { EmailPasswordPreBuiltUI } from "supertokens-auth-react/recipe/emailpassword/prebuiltui";
 
 if (typeof window !== "undefined") {
   // we only want to call this init function on the frontend, so we check typeof window !== 'undefined'
@@ -20,16 +16,6 @@ export const SuperTokensProvider: React.FC<React.PropsWithChildren<{}>> = ({
   children,
 }) => {
   setRouter(useRouter(), usePathname() || window.location.pathname);
-  if (
-    canHandleRoute([
-      /* Other pre built UI */ EmailPasswordPreBuiltUI,
-      EmailVerificationPreBuiltUI,
-    ])
-  ) {
-    return getRoutingComponent([
-      EmailPasswordPreBuiltUI,
-      /* Other pre built UI */ EmailVerificationPreBuiltUI,
-    ]);
-  }
+
   return <SuperTokensWrapper>{children}</SuperTokensWrapper>;
 };
